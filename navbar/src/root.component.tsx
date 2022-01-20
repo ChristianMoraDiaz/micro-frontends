cd7a465f21b16acb52d197b605314c375fae423a
ChristianMoraDiaz <christianmoradiaz@hotmail.com>
2022-01-20 17:36:06 -0500

    feat(Core): Added navbar micro-frontend

---
+++ navbar/src/root.component.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./root.component.css";
+
+export default function Root() {
+  return (
+    <nav className="nav">
+      <a href="/" className="link">
+        Home
+      </a>
+      <a href="/faq" className="link">
+        FAQ
+      </a>
+      <a href="/about-us" className="link">
+        About us
+      </a>
+      <a href="/sign-in" className="link">
+        Sign In
+      </a>
+    </nav>
+  );
+}
